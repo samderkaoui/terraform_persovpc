0dba3bedc15dea618811a89990e236eb487a2919
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 16:09:38 +0200

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat[0].id
+  condition = length(var.public_subnet_cidrs) > 0
+}
