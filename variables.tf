e307973445e1a6c88059bd4dec867888914bde54
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 16:09:22 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "A list of availability zones in the region"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "A list of CIDR blocks for the private subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "A list of CIDR blocks for the public subnets"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
