faabb1067374aa9ef61c1dd84540c4ed76fbf5f6
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 16:26:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,9 +40,9 @@ resource "aws_subnet" "private" {
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
 
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.public_subnet_cidrs, count.index)
-  availability_zone = element(var.azs, count.index)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = element(var.azs, count.index)
   map_public_ip_on_launch = true
 
   tags = merge(
@@ -54,20 +54,21 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  count = length(var.public_subnet_cidrs) > 0 ? 1 : 0
+  count = length(var.public_subnet_cidrs)
 
-  allocation_id = aws_eip.nat.id
-  subnet_id     = element(aws_subnet.public.*.id, 0)
+  allocation_id = element(aws_eip.nat.*.id, count.index)
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
 
   tags = merge(
     var.tags,
     {
-      Name = "${var.vpc_name}-nat"
+      Name = "${var.vpc_name}-nat-${count.index}"
     }
   )
 }
 
 resource "aws_eip" "nat" {
+  count  = length(var.public_subnet_cidrs)
   domain = "vpc"
 }
 
@@ -91,8 +92,8 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = length(var.public_subnet_cidrs) > 0 ? aws_nat_gateway.nat[0].id : null
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.nat.*.id, 0)  # Utilise le premier NAT Gateway comme fallback
   }
 
   tags = merge(
