60b6cec9b6dd7f9d1ccde562caa7c7ca775b35f0
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 16:32:17 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,6 @@ output "private_subnets" {
   value = aws_subnet.private[*].id
 }
 
-output "nat_gateway_id" {
-  value = length(var.public_subnet_cidrs) > 0 ? aws_nat_gateway.nat[0].id : null
+output "nat_gateway_ids" {
+  value = aws_nat_gateway.nat[*].id
 }
